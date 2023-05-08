5bab2100e4096a0650d7ae13af40d7ba04f6e36a
RajonDawn <118959146+RajonDawn@users.noreply.github.com>
2023-05-08 22:35:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import numpy as np
 import pandas as pd
 import altair as alt
 import streamlit as st
+from io import BytesIO
+from pyxlsb import open_workbook as open_xlsb
+
 
 st.header('FFA VOC Collection Template')
 st.subheader('CCI CE')
