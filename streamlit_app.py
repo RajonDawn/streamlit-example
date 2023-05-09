7b5903df02750e34655f347881f90512bf952b80
RajonDawn <118959146+RajonDawn@users.noreply.github.com>
2023-05-09 15:11:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,33 +51,37 @@ for i in [climate, road, landform, grade, sit, specEnv, specWei, specLeg, specAt
     rows = rows * 1
 st.write(f'输出组合种类数量为: {rows}')
 
-output = pd.DataFrame(columns=['一级细分市场',	'二级细分市场',	'三级细分市场',	'关注度',	'气候特征',	'道路/地形', '地貌特征',	'坡度',	'载重',	'特殊环境',	'特殊法规',	'细分工况'])
+@st.cache_data
+def fileGen(specAtt, climate, road, landform, grade, specEnv, specWei, specLeg, sit):
+  output = pd.DataFrame(columns=['一级细分市场',	'二级细分市场',	'三级细分市场',	'关注度',	'气候特征',	'道路/地形', '地貌特征',	'坡度',	'载重',	'特殊环境',	'特殊法规',	'细分工况'])
 
-# @st.cache()
-# def fileGen():
-#   for att in specAtt:
-#     for temp_climate in climate:
-#       for temp_road in road:
-#         for temp_landform in landform:
-#           for temp_grade in grade:
-#             for env in specEnv:
-#               for wei in specWei:
-#                 for leg in specLeg:
-#                     for temp_sit in sit:
-#                       temp = pd.DataFrame({ '一级细分市场': [lev1],
-#                                             '二级细分市场': [lev2],
-#                                             '三级细分市场': [lev3],
-#                                             '关注度':[att],
-#                                             '气候特征':  [temp_climate],
-#                                             '道路/地形': [temp_road],
-#                                             '地貌特征': [temp_landform],
-#                                             '坡度':   [temp_grade],
-#                                             '载重':	[wei],
-#                                             '特殊环境':	[env],
-#                                             '特殊法规': [leg],
-#                                             '细分工况': [temp_sit]      })
-#                       output = pd.concat([output,temp])
-#   return output
-  
-# if st.download_button('📥下载VOC表格', data=fileGen().to_csv(index=False).encode('utf_8_sig'),file_name='VOC Collection Table.csv',mime='text/csv'):
-#   st.success( lev1 +' - '+ lev2 +' - ' + lev3 + '- voc collection table finished!', icon="✅")
+  for att in specAtt:
+    for temp_climate in climate:
+      for temp_road in road:
+        for temp_landform in landform:
+          for temp_grade in grade:
+            for env in specEnv:
+              for wei in specWei:
+                for leg in specLeg:
+                    for temp_sit in sit:
+                      temp = pd.DataFrame({ '一级细分市场': [lev1],
+                                            '二级细分市场': [lev2],
+                                            '三级细分市场': [lev3],
+                                            '关注度':[att],
+                                            '气候特征':  [temp_climate],
+                                            '道路/地形': [temp_road],
+                                            '地貌特征': [temp_landform],
+                                            '坡度':   [temp_grade],
+                                            '载重':	[wei],
+                                            '特殊环境':	[env],
+                                            '特殊法规': [leg],
+                                            '细分工况': [temp_sit]      })
+                      output = pd.concat([output,temp])
+
+
+  return output
+
+
+if st.download_button('📥下载VOC表格', data=fileGen(specAtt, climate, road, landform, grade, specEnv, specWei, specLeg, sit).to_csv(index=False).encode('utf_8_sig'),file_name='VOC Collection Table.csv',mime='text/csv'):
+  st.balloons()
+  st.success( lev1 +' - '+ lev2 +' - ' + lev3 + '- voc collection table completed!', icon="✅")
