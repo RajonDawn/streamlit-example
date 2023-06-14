1f6948d0222c476e5202279d1e6a08b12702344b
RajonDawn <118959146+RajonDawn@users.noreply.github.com>
2023-06-14 14:36:39 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,31 @@ import streamlit as st
 from io import BytesIO
 from io import StringIO
 
-dataSet = pd.read_excel('./FFA场景定义 template 20230508.xlsx', sheet_name=None)
+
+
+
+    
+@st.cache_data
+def init_data(file = './FFA场景定义 template 20230508.xlsx'):
+  inData = pd.read_excel(file, sheet_name=None)
+  return inData
+
+dataSet = init_data()
+
 st.header('FFA VOC Collection Template')
 st.subheader('CCI CE')
 
 # Silder
 with st.sidebar:
-  uploaded_file = st.file_uploader("请上传FFA模板文件", accept_multiple_files=False)
+  uploaded_file = st.file_uploader("请上传FFA模板文件", accept_multiple_files=False )
+
   if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
-    dataSet = pd.read_excel(uploaded_file, sheet_name=None)
+    dataSet = init_data(uploaded_file, sheet_name=None)
   sheetNames = list(dataSet)
 
   st.header('Application')
-  lev0 = st.selectbox(label='项目名称', options=sheetNames)
+  lev0 = st.selectbox(label='数据表名', options=sheetNames)
   tempSheet = dataSet[lev0]
   lev1 = st.selectbox(label='一级细分市场', options = tempSheet.iloc[:, 0].dropna()) 
   lev2 = st.selectbox(label='二级细分市场', options = tempSheet.iloc[:, 1].dropna()) 
@@ -27,21 +38,20 @@ st.empty()
 col1, col2 = st.columns(2)
 with col1:
   climate = st.multiselect(label='气候特征', options = tempSheet['气候特征'].dropna()) 
-  st.image('./climate.jpg', use_column_width='always')
+  st.image('./images/climate.jpg', use_column_width='always')
   road = st.multiselect(label='道路/地形', options = tempSheet['道路/地形'].dropna()) 
-  st.image('./road.jpg', use_column_width='always')
+  st.image('./images/road.jpg', use_column_width='always')
 with col2:
   landform = st.multiselect(label='地貌特征', options = tempSheet['地貌特征'].dropna()) 
-  st.image('./landform.jpg', use_column_width='always')
+  st.image('./images/landform.jpg', use_column_width='always')
   grade = st.multiselect(label='坡度', options = tempSheet['坡度'].dropna()) 
-  st.image('./grade.jpg', use_column_width='always')
+  st.image('./images/grade.jpg', use_column_width='always')
 sit = st.multiselect(label='细分工况', options = tempSheet['细分工况'].dropna(), default=tempSheet['细分工况'].dropna()) 
 
 specEnv = st.multiselect(label='特殊环境', options=tempSheet['特殊环境'].dropna())
 specWei = st.multiselect(label='载重', options = tempSheet['载重'].dropna())
 specLeg = st.multiselect(label='特殊法规', options = tempSheet['特殊法规'].dropna())
 specAtt = st.multiselect(label='关注度', options = tempSheet['关注度'].dropna())
-
 rows = 1
 for i in [climate, road, landform, grade, sit, specEnv, specWei, specLeg, specAtt]:
   if len(i)>=1:
@@ -77,7 +87,10 @@ def fileGen(specAtt, climate, road, landform, grade, specEnv, specWei, specLeg, 
                                             '特殊法规': [leg],
                                             '细分工况': [temp_sit]      })
                       output = pd.concat([output,temp])
+
+
   return output
+
 
 if st.download_button('📥下载VOC表格', data=fileGen(specAtt, climate, road, landform, grade, specEnv, specWei, specLeg, sit).to_csv(index=False).encode('utf_8_sig'),file_name='VOC Collection Table.csv',mime='text/csv'):
   st.balloons()
@@ -85,3 +98,6 @@ if st.download_button('📥下载VOC表格', data=fileGen(specAtt, climate, road
     st.success( lev1 +' - '+ lev2 +' - ' + lev3 + '- voc collection table completed!', icon="✅")
   else:
     st.success( lev1 +' - '+ lev2 + '- voc collection table completed!', icon="✅")
+
+
+
